507175ebc02fde1b24bfca9a85dfe56d0a9f9652
AmandaBarros94 <mandy.bjj1@gmail.com>
2022-10-31 16:44:48 -0300

    Testando o relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    mock_product = Product(
+        1,
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        "2021-02-18",
+        "2023-09-17",
+        "CR25 1551 4467 2549 4402 1",
+        "instrucao 1",
+    )
+
+    assert str(mock_product) == (
+        f"O produto {mock_product.nome_do_produto}"
+        f" fabricado em {mock_product.data_de_fabricacao}"
+        f" por {mock_product.nome_da_empresa} com validade"
+        f" até {mock_product.data_de_validade}"
+        f" precisa ser armazenado {mock_product.instrucoes_de_armazenamento}."
+    )
